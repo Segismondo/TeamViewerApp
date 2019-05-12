bc29f2e989b0884e98842fcfa42f812918884133
Segismondo <adri10alamo@gmail.com>
2019-05-12 21:31:32 +0200

    First commit

---
+++ src/main/java/com/adrian/teamviewer/TeamviewerApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.teamviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamviewerApplication.class, args);
+	}
+
+}
